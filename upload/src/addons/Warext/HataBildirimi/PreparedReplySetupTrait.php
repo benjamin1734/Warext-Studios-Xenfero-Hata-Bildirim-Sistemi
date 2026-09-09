c34012fca74b8a4c8c230779f5107c275d7c427e
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-09-09 13:17:41 +0300

    Add prepared reply category schema upgrade

--- upload/src/addons/Warext/HataBildirimi/PreparedReplySetupTrait.php
+++ upload/src/addons/Warext/HataBildirimi/PreparedReplySetupTrait.php
@@ -2,6 +2,7 @@
 
 namespace Warext\HataBildirimi;
 
+use XF\Db\Schema\Alter;
 use XF\Db\Schema\Create;
 
 trait PreparedReplySetupTrait
@@ -11,9 +12,28 @@ trait PreparedReplySetupTrait
         $this->createPreparedReplyTable();
     }
 
+    public function installStep6(): void
+    {
+        $this->createPreparedReplyCategoryTable();
+    }
+
     public function upgrade1000180Step1(): void
     {
         $this->createPreparedReplyTable();
+    }
+
+    public function upgrade1010000Step1(): void
+    {
+        $this->createPreparedReplyCategoryTable();
+    }
+
+    public function upgrade1010000Step2(): void
+    {
+        $this->schemaManager()->alterTable('xf_wrxt_bug_prepared_reply', function(Alter $table)
+        {
+            $table->addColumn('prepared_reply_category_id', 'int')->unsigned()->setDefault(0)->after('prepared_reply_id');
+            $table->addKey(['prepared_reply_category_id', 'display_order'], 'category_order');
+        });
     }
 
     protected function createPreparedReplyTable(): void
@@ -21,6 +41,7 @@ trait PreparedReplySetupTrait
         $this->schemaManager()->createTable('xf_wrxt_bug_prepared_reply', function(Create $table)
         {
             $table->addColumn('prepared_reply_id', 'int')->unsigned()->autoIncrement();
+            $table->addColumn('prepared_reply_category_id', 'int')->unsigned()->setDefault(0);
             $table->addColumn('title', 'varchar', 100);
             $table->addColumn('message', 'mediumtext');
             $table->addColumn('display_order', 'int')->unsigned()->setDefault(10);
@@ -29,11 +50,27 @@ trait PreparedReplySetupTrait
             $table->addColumn('updated_date', 'int')->unsigned();
             $table->addPrimaryKey('prepared_reply_id');
             $table->addKey(['active', 'display_order']);
+            $table->addKey(['prepared_reply_category_id', 'display_order'], 'category_order');
+        });
+    }
+
+    protected function createPreparedReplyCategoryTable(): void
+    {
+        $this->schemaManager()->createTable('xf_wrxt_bug_prepared_reply_category', function(Create $table)
+        {
+            $table->addColumn('prepared_reply_category_id', 'int')->unsigned()->autoIncrement();
+            $table->addColumn('title', 'varchar', 100);
+            $table->addColumn('display_order', 'int')->unsigned()->setDefault(10);
+            $table->addColumn('created_date', 'int')->unsigned();
+            $table->addColumn('updated_date', 'int')->unsigned();
+            $table->addPrimaryKey('prepared_reply_category_id');
+            $table->addKey(['display_order', 'title']);
         });
     }
 
     public function uninstallStep4(): void
     {
         $this->schemaManager()->dropTable('xf_wrxt_bug_prepared_reply');
+        $this->schemaManager()->dropTable('xf_wrxt_bug_prepared_reply_category');
     }
 }
